72c642e8319b37ed70fa2c78521a85e6c2dd6cde
Chaitanya Dhamdhere <chaitu0608@gmail.com>
2025-04-22 14:29:21 +0530

    PUSH1

--- public/homework.php
+++ public/homework.php
@@ -93,7 +93,7 @@ $homeworks = $hwQuery->fetchAll(PDO::FETCH_OBJ);
                 </a>
             </nav>
             <div class="absolute bottom-0 w-full p-4 border-t border-dark-border">
-                <a href="#" class="flex items-center px-4 py-3 text-gray-400 rounded-md hover:bg-dark-border hover:text-white">
+                <a href="./logout.php" class="flex items-center px-4 py-3 text-gray-400 rounded-md hover:bg-dark-border hover:text-white">
                     <i class="fas fa-sign-out-alt w-5 h-5"></i>
                     <span class="ml-3">Logout</span>
                 </a>
